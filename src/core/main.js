a2b508364ea5f513e8f5a0937fe3bc44afdd25c5
Kursant <Kursant@BOOK3778FZ.win.ftc.ru>
2018-10-15 17:03:32 +0700

    initial commit

---
+++ src/core/main.js
@@ -0,0 +1,9 @@
+import '../tasks/tasks';
+
+console.log('main.js');
+
+class Foo {
+    bar = 123;
+}
+
+console.log(new Foo().bar);
---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackConfig = require('html-webpack-plugin');
+
+module.exports = (env, argv) => ({
+    entry: ['./src/core/main.js', './src/core/main.scss'],
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    devtool: 'souce-map',
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ['@babel/preset-env'],
+                        plugins: ['@babel/plugin-proposal-class-properties']
+                    }
+                }
+            },
+            {
+                test: /\.s?css$/,
+                use: [
+                    'css-hot-loader',
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'resolve-url-loader',
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sourseMap: true,
+                            sourceMapContents: false
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(png|jpg|gif)$/,
+                use: ['file-loader']
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin('dist'),
+        new MiniCssExtractPlugin({ filename: 'bundle.css' }),
+        new HtmlWebpackConfig({
+            template: './src/core/index.html',
+            hash: true,
+            minify: {
+                collapseWhitespace: true,
+                html5: true,
+                removeComments: true,
+                removeEmptyAttributes: true
+            }
+        })
+    ]
+});
